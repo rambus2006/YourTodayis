c30bc6bbfc24c71fe16a29e47227fee72964266a
rambus2006 <w2227@e-mirim.hs.kr>
2024-04-04 18:44:51 +0900

    일기 사이드 바 및 배경 부분작업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useEffect,useRef,useState} from 'react';
+
+
+
+// function Sidebar
+const Sidebar = ()=>{
+  return(
+    // <!-- Side navigation -->
+  <div class="sidenav">
+    <div className='couple_img'>
+      <img className="coupleimg" src="./asset/coupleimg.png"/>
+    </div>
+    <a href="#">디데이</a>
+    <a href="#">일기장 제목</a>
+    <a href="#">일기장 설명</a>
+    <a href="#" id='bottom_menu'>시작날짜</a>
+    <a href="#" id='bottom_menu'>현재날짜</a>
+
+  </div>
+  )
+} 
+const Bgimg = () =>{
+  return <img src='./asset/Bgimg.png'></img>
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <>
+      <Bgimg />
+      
+      <Sidebar/>
+    </>
+    
+  ) 
+  
 }
 
 export default App;
